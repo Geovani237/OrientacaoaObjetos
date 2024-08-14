420164dad307181b2573ba1ff355352149cd534e
Geovani Carlos de Souza <geovanicsouza7@gmail.com>
2024-08-14 10:29:40 -0300

    Classes e Objetos
    aprendendo com o conteúdo da FIAP

---
+++ OrientacaoaObjeto/src/projetoFiap/HabilidadeEspecial.java
@@ -0,0 +1,14 @@
+package projetoFiap;
+
+public class HabilidadeEspecial {
+    String nome;
+    int custoEnergia;
+    boolean habilitada;
+
+    public HabilidadeEspecial(String nome, int custoEnergia, boolean habilitada) {
+        this.nome = nome;
+        this.custoEnergia = custoEnergia;
+        this.habilitada = habilitada;
+    }
+    
+}
---
+++ OrientacaoaObjeto/src/projetoFiap/PersonagemMagico.java
@@ -0,0 +1,19 @@
+package projetoFiap;
+
+public class PersonagemMagico {
+
+    //Atributos
+    String nome;
+    String poderMagico;
+    int nivelEnergia;
+
+    HabilidadeEspecial habilidade;
+
+    PersonagemMagico() {
+
+    }
+
+    public PersonagemMagico(String nome) {
+        this.nome = nome;
+    }
+}
